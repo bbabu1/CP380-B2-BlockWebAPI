7490fecdfdf0473999c2a67e2d33e26acdfc0afe
BONY BABU <65872018+bbabu1@users.noreply.github.com>
2021-08-18 14:01:46 -0400

    Add files via upload

---
+++ CP380-B2-BlockWebAPI/CP380-B2-BlockWebAPI/Controllers/PendingPayloadsController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CP380_B2_BlockWebAPI.Models;
+using CP380_B1_BlockList.Models;
+
+namespace CP380_B2_BlockWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    
+    
+    
+    public class PendingPayloadsController : ControllerBase
+    {
+     
+        private readonly PendingPayloads _pendingPayloads;
+        public List<PendingPayloads> pendingPayloadsList { get; set; }
+
+        
+        
+        
+        public PendingPayloadsController(PendingPayloads pendingPayloads)
+       
+        
+        
+        
+        {
+            _pendingPayloads = pendingPayloads;
+        }
+
+      
+        
+        
+        [HttpGet]
+       
+        
+        public ActionResult<List<Payload>> Get()
+        {
+            return _pendingPayloads.payloads.ToList();
+        }
+
+
+        [HttpPost]
+       
+        
+        public void Post(Payload payload)
+        {
+            _pendingPayloads.payloads.Add(payload);
+
+        }
+}
+}
---
+++ CP380-B2-BlockWebAPI/CP380-B2-BlockWebAPI/Models/BlockSummary.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CP380_B1_BlockList.Models;
+
+namespace CP380_B2_BlockWebAPI.Models
+{
+    public class BlockSummary
+    {
+       
+        public DateTime TimeStamp { get; set; }
+        public string PreviousHash { get; set; }
+        public string Hash { get; set; }
+
+       
+        
+        
+        public BlockSummary()
+       
+        
+        
+        {
+           
+            List<Payload> data12 = new()
+            {
+                new Payload("user", TransactionTypes.GRANT, 10, null),
+                new Payload("user", TransactionTypes.BUY, 10, "10C"),
+            };
+          
+            
+            var block = new Block(DateTime.Now, "", data12);
+            block.Mine(2);
+
+            TimeStamp = block.TimeStamp;
+            PreviousHash = block.PreviousHash;
+            Hash = block.Hash;
+        }
+}
+
+}
