d92e4dc8ccb958deb3a522ec90724b7d4dc4c035
BONY BABU <65872018+bbabu1@users.noreply.github.com>
2021-08-19 18:24:23 -0400

    Add files via upload

---
+++ CP380-B2-BlockWebAPI/Services/BlockListServices.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CP380_B1_BlockList.Models;
+using CP380_B2_BlockWebAPI.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace CP380_B2_BlockWebAPI.Services
+{
+    public class BlockListService
+    {
+
+        private readonly IConfiguration _config;
+
+        public BlockList blockList { get; set; }
+        public PendingPayloads pendingPayloads { get; set; }
+
+        public List<Payload> payloadList { get; set; }
+
+        public BlockListService(IConfiguration configuration, BlockList blockList, PendingPayloads pendingPayloads)
+        {
+            _config = configuration;
+            this.blockList = blockList;
+            this.pendingPayloads = pendingPayloads;
+        }
+
+        public Block SubmitNewBlock(string hash, int nonce, DateTime timestamp)
+        {
+            payloadList = pendingPayloads.payloads;
+            var tmpBlockList = blockList.Chain[blockList.Chain.Count - 1];
+            var blk = new Block(tmpBlockList.TimeStamp, tmpBlockList.PreviousHash, payloadList);
+            if(blk.CalculateHash() == hash)
+            {
+                blockList.Chain.Add(blk);
+                payloadList.Clear();
+                return blk;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
